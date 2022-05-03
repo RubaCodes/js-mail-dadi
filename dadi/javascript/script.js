07b8460626ff834d516303c78d347970e980d4b6
rubacodes <rubacodes@gmail.com>
2022-05-03 13:51:18 +0200

    aggiunto calcolo numero random e logica

--- dadi/javascript/script.js
+++ dadi/javascript/script.js
@@ -1,1 +1,16 @@
-alert('hello world')
+
+
+//generazione numeri casuali
+let userNumber = Math.floor(Math.random() * 6) + 1;
+let computerNumber = Math.floor(Math.random() * 6) + 1;
+console.log(userNumber, computerNumber);
+
+
+// logica vittoria
+if(userNumber > computerNumber){
+    // vittoria user
+} else if(userNumber < computerNumber){
+    //vittoria pc
+}else {
+    //pareggio
+}
