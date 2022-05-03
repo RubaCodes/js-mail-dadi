fe0c315877e160fbc792b06d50beee34cd644b6a
rubacodes <rubacodes@gmail.com>
2022-05-03 14:21:11 +0200

    tolto codice duplicato

--- dadi/javascript/script.js
+++ dadi/javascript/script.js
@@ -18,18 +18,3 @@ refreshButton.addEventListener('click',
         }
     }
 );
-
-//generazione numeri casuali
-let userNumber = Math.floor(Math.random() * 6) + 1;
-let computerNumber = Math.floor(Math.random() * 6) + 1;
-console.log(userNumber, computerNumber);
-
-
-// logica vittoria
-if(userNumber > computerNumber){
-    console.log('vittoria user');
-} else if(userNumber < computerNumber){
-    console.log('vittoria pc');
-}else {
-    console.log('pareggio');
-}
