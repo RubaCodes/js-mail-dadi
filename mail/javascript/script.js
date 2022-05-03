832be4870b50bae2795e507f4814a572597b4cf8
rubacodes <rubacodes@gmail.com>
2022-05-03 16:16:19 +0200

    aggiunta lista e click login

---
+++ mail/javascript/script.js
@@ -0,0 +1,9 @@
+const emailList = ['dahdiua@gmail.com', 'adsbabda@outlook.com', 'adahskdj@libero.it','test.gmail.com'];
+
+const login = document.querySelector('login');
+
+login.addEventListener('click', 
+    function(){
+
+    }
+);
