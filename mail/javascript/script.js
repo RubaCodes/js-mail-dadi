0c31eac8f17441b1ca75b4171860a1126de85244
rubacodes <rubacodes@gmail.com>
2022-05-03 16:25:00 +0200

    primo check dell' email ,errato

--- mail/javascript/script.js
+++ mail/javascript/script.js
@@ -1,9 +1,17 @@
-const emailList = ['dahdiua@gmail.com', 'adsbabda@outlook.com', 'adahskdj@libero.it','test.gmail.com'];
+const emailList = ['dahdiua@gmail.com', 'adsbabda@outlook.com', 'adahskdj@libero.it', 'test@gmail.com'];
 
-const login = document.querySelector('login');
+const login = document.querySelector('#login');
 
-login.addEventListener('click', 
-    function(){
-
+login.addEventListener('click',
+    function () {
+        const userEmail = document.querySelector('#email').value;
+        console.log(userEmail);
+        for (let i = 0; i < emailList.length; i++){
+            if(userEmail != emailList[i]){
+            console.log('username errato');
+            }else{
+                console.log('utente loggato');
+            }
+        }
     }
 );
