f766ea3cdd7965a5431e50ec5b0c4ede7c2bcb54
rubacodes <rubacodes@gmail.com>
2022-05-03 14:56:49 +0200

    tentativo di cambiare faccia al dado

--- dadi/javascript/script.js
+++ dadi/javascript/script.js
@@ -1,21 +1,26 @@
 const refreshButton = document.querySelector('#refresh-button');
 
 
-refreshButton.addEventListener('click', 
-    function(){
+refreshButton.addEventListener('click',
+    function () {
         //generazione numeri casuali
         let userNumber = Math.floor(Math.random() * 6) + 1;
         let computerNumber = Math.floor(Math.random() * 6) + 1;
+        //cambio faccia dado
+        const userDice = document.querySelector('.col.user .dice');
+        const computerDice = document.querySelector('.col.pc .dice');
+        userDice.className = ` dice dice-${userNumber}`;
+        computerDice.className = ` dice dice-${computerNumber}`;
         //display del numero sorteggiato
         document.querySelector('#user-number').innerHTML = `Il tuo numero estratto per te e' ${userNumber}`;
         document.querySelector('#computer-number').innerHTML = `Il  numero estratto per il pc e' ${computerNumber}`;
 
         // logica vittoria
-        if(userNumber > computerNumber){
+        if (userNumber > computerNumber) {
             document.querySelector('#result').innerHTML = `HAI VINTO`;
-        } else if(userNumber < computerNumber){
+        } else if (userNumber < computerNumber) {
             document.querySelector('#result').innerHTML = `HAI PERSO`;
-        }else {
+        } else {
             document.querySelector('#result').innerHTML = `PAREGGIO`;
         }
     }
