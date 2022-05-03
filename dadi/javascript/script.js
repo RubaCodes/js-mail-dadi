3b89c304ca1c70c9c4f112e4d86c6cdf415520ae
rubacodes <rubacodes@gmail.com>
2022-05-03 15:21:43 +0200

    rinominato le img

--- dadi/javascript/script.js
+++ dadi/javascript/script.js
@@ -9,9 +9,10 @@ refreshButton.addEventListener('click',
         //cambio faccia dado
         const userDice = document.querySelector('.col.user .dice');
         const computerDice = document.querySelector('.col.pc .dice');
-        userDice.style.backgroundImage = `url('img/dice-${userNumber}-solid.svg')`;
-        computerDice.style.backgroundImage = `url('img/dice-${computerNumber}-solid.svg')`;
-        
+        userDice.style.backgroundImage = `url('../img/dice-${userNumber}-solid.svg')`;
+        computerDice.style.backgroundImage = `url('../img/dice-${computerNumber}-solid.svg')`;
+        userDice.style.backgroundSize = 'cover';
+        computerDice.style.backgroundSize = 'cover';
         //display del numero sorteggiato
         document.querySelector('#user-number').innerHTML = `Il tuo numero estratto per te e' ${userNumber}`;
         document.querySelector('#computer-number').innerHTML = `Il  numero estratto per il pc e' ${computerNumber}`;
