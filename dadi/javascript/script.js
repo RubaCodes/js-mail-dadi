679e3de37c715d1e4fec8311217e2e30a4fa9a55
rubacodes <rubacodes@gmail.com>
2022-05-03 14:20:47 +0200

    spostato l'esecuzione del programma al click

--- dadi/javascript/script.js
+++ dadi/javascript/script.js
@@ -1,4 +1,23 @@
+const refreshButton = document.querySelector('#refresh-button');
 
+refreshButton.addEventListener('click', 
+    function(){
+        //generazione numeri casuali
+        let userNumber = Math.floor(Math.random() * 6) + 1;
+        let computerNumber = Math.floor(Math.random() * 6) + 1;
+        console.log(userNumber, computerNumber);
+
+
+        // logica vittoria
+        if(userNumber > computerNumber){
+            console.log('vittoria user');
+        } else if(userNumber < computerNumber){
+            console.log('vittoria pc');
+        }else {
+            console.log('pareggio');
+        }
+    }
+);
 
 //generazione numeri casuali
 let userNumber = Math.floor(Math.random() * 6) + 1;
