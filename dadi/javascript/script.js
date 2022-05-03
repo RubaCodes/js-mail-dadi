cdca521ecf068eaeb40470eb14f2686b9752bd6f
rubacodes <rubacodes@gmail.com>
2022-05-03 14:27:35 +0200

    display del numero sorteggiato su html

--- dadi/javascript/script.js
+++ dadi/javascript/script.js
@@ -1,11 +1,15 @@
 const refreshButton = document.querySelector('#refresh-button');
 
+
 refreshButton.addEventListener('click', 
     function(){
         //generazione numeri casuali
         let userNumber = Math.floor(Math.random() * 6) + 1;
         let computerNumber = Math.floor(Math.random() * 6) + 1;
-        console.log(userNumber, computerNumber);
+        //display del numero sorteggiato
+        document.querySelector('#user-number').innerHTML = `Il tuo numero estratto per te e' ${userNumber}`;
+        document.querySelector('#computer-number').innerHTML = `Il  numero estratto per il pc e' ${computerNumber}`;
+
 
 
         // logica vittoria
